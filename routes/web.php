1cfd42a42ec8d1918a32e250f61456089a684d14
drewheathens <mudavadie@gmail.com>
2020-03-29 20:11:33 +0300

    initial commit crud working

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Contact;
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/','ContactController@index') ->name('index');
+Route::resource('contacts','ContactController');
